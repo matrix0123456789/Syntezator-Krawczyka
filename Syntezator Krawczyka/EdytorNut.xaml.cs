ba9ed3eb9a00d59ab322f422c9dee908cbf92aea
matrix0123456789 <Matrix0123456789@tlen.pl>
2014-01-04 13:39:26 +0100

    Edytor nut

--- Syntezator Krawczyka/EdytorNut.xaml.cs
+++ Syntezator Krawczyka/EdytorNut.xaml.cs
@@ -25,8 +25,8 @@ namespace Syntezator_Krawczyka
             main = input;
             if (input.nuty.Count > 0)
             {
-                const double skalaX=10;
-                const double skalaY=10;
+                const double skalaX = 20;
+                const double skalaY = 20;
                 double ilepróbekMin, ilepróbekMax;
                 ilepróbekMax = ilepróbekMin = input.nuty[0].ilepróbekNaStarcie;
                 foreach (var x in input.nuty)
@@ -40,22 +40,56 @@ namespace Syntezator_Krawczyka
                         ilepróbekMin = x.ilepróbekNaStarcie;
                     }
                 }
-               // double tonMin=funkcje.ton(ilepróbekMin)-funkcje.ton(ilepróbekMax)
-                double tonMin=funkcje.ton(ilepróbekMin);
-                double tonMax=funkcje.ton(ilepróbekMax);
+                // double tonMin=funkcje.ton(ilepróbekMin)-funkcje.ton(ilepróbekMax)
+                double tonMin = funkcje.ton(ilepróbekMin)+1;
+                double tonMax = funkcje.ton(ilepróbekMax)-1;
+                for (var i = tonMin; i >= tonMax; i = i - .5)
+                {
+                    var tonTeraz = Math.Round((i+600) % 6, 2);//+600 żeby dla ujemnych nie sprawdzał w odwrotnej kolejności
+                    if (tonTeraz == .5 || tonTeraz == 1.5 || tonTeraz == 3 || tonTeraz == 4 || tonTeraz == 5)
+                    {
+                        var prostokat = new Rectangle();
+                        prostokat.Margin = new Thickness(0, (tonMin - i) * skalaY, 0, 0);
+                        prostokat.Height = (skalaY / 2);
+                        prostokat.Fill = Brushes.Beige;
+                        prostokat.VerticalAlignment = System.Windows.VerticalAlignment.Top;
+                        prostokat.HorizontalAlignment = System.Windows.HorizontalAlignment.Stretch;
+                        panel.Children.Add(prostokat);
+                    } else if (tonTeraz == 0)//oktawa
+                    {
+                        var prostokat = new Rectangle();
+                        prostokat.Margin = new Thickness(0, (tonMin - i) * skalaY, 0, 0);
+                        prostokat.Height = (1);
+                        prostokat.Fill = Brushes.Black;
+                        prostokat.VerticalAlignment = System.Windows.VerticalAlignment.Top;
+                        prostokat.HorizontalAlignment = System.Windows.HorizontalAlignment.Stretch;
+                        panel.Children.Add(prostokat);
+                    }
+                }
                 foreach (var x in input.nuty)
                 {
                     var prostokat = new Rectangle();
-                    prostokat.Margin = new Thickness((plik.tempo * x.opuznienie / (60 * plik.Hz) * skalaX), (tonMin - funkcje.ton(x.ilepróbekNaStarcie)) * skalaY, 0, 0);
+                    prostokat.Margin = new Thickness((plik.tempo * (x.opuznienie - input.delay) / (60 * plik.Hz) * skalaX), (tonMin - funkcje.ton(x.ilepróbekNaStarcie)) * skalaY, 0, 0);
 
-                        prostokat.Width = plik.tempo * x.długość / (60*plik.Hz) * skalaX;
-                    prostokat.Height=( skalaY / 2);
+                    prostokat.Width = plik.tempo * x.długość / (60 * plik.Hz) * skalaX;
+                    prostokat.Height = (skalaY / 2);
                     prostokat.Fill = Brushes.Red;
                     prostokat.VerticalAlignment = System.Windows.VerticalAlignment.Top;
                     prostokat.HorizontalAlignment = System.Windows.HorizontalAlignment.Left;
+                    prostokat.Tag = x;
+                    prostokat.MouseEnter += prostokat_MouseEnter;
                     panel.Children.Add(prostokat);
                 }
             }
         }
+
+        void prostokat_MouseEnter(object sender, MouseEventArgs e)
+        {
+            aktywna = (Rectangle)sender;
+            var nuta = ((sender as Rectangle).Tag as nuta);
+
+        }
+
+        public Rectangle aktywna { get; set; }
     }
 }
