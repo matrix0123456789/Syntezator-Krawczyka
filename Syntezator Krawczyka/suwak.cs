d4e013f4cf18c2096982520fb9da4e36adb41a9c
matrix0123456789 <Matrix0123456789@tlen.pl>
2014-10-26 11:58:25 +0100

    Poprawa suwaków (skala kwadratowa)

--- Syntezator Krawczyka/suwak.cs
+++ Syntezator Krawczyka/suwak.cs
@@ -8,6 +8,7 @@ using System.Windows.Controls;
 
 namespace Syntezator_Krawczyka
 {
+    public enum Przelicznik : byte { Liniowo, Logarytmicznie, Kwadratowo }
     public class suwak : Slider
     {
         [Bindable(true)]
@@ -20,11 +21,25 @@ namespace Syntezator_Krawczyka
             }
             set
             {
-                ToolTip = value + "\r\n" + this.Value+_jednostka;
+                ToolTip = value + "\r\n" + Value + _jednostka;
                 _opis = value;
             }
         }
         String _jednostka = "";
+        public Przelicznik _przelicznik;
+        [Bindable(true)]
+        [Category("Własne")]
+        public Przelicznik przelicznik
+        {
+            get { return _przelicznik; }
+            set
+            {
+                _przelicznik = value;
+                maksimumPrzelicz(_maks);
+
+            }
+        }
+
         [Bindable(true)]
         [Category("Własne")]
         public string Jednostka
@@ -35,13 +50,14 @@ namespace Syntezator_Krawczyka
             }
             set
             {
-                ToolTip = _opis + "\r\n" + this.Value+value;
+                ToolTip = _opis + "\r\n" + Value + value;
                 _jednostka = value;
             }
         }
         string _opis;
         public suwak()
         {
+            przelicznik = Przelicznik.Liniowo;
             ValueChanged += suwak_ValueChanged;
             MouseRightButtonDown += suwak_MouseRightButtonDown;
         }
@@ -55,9 +71,137 @@ namespace Syntezator_Krawczyka
         void suwak_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
         {
             var test = sender == this;
-            ToolTip = _opis + "\r\n" + base.Value+Jednostka;
+            ToolTip = _opis + "\r\n" + Value + Jednostka;
 
 
+        }
+        public double Value
+        {
+            get
+            {
+                switch (przelicznik)
+                {
+                    case Przelicznik.Liniowo:
+                        return base.Value;
+                    case Przelicznik.Kwadratowo:
+                        return base.Value * base.Value;
+                    case Przelicznik.Logarytmicznie:
+                        return Math.Pow(10, base.Value);
+                    default:
+                        return base.Value;
+                }
+            }
+            set
+            {
+                switch (przelicznik)
+                {
+                    case Przelicznik.Liniowo:
+                        base.Value = value;
+                        break;
+                    case Przelicznik.Kwadratowo:
+                        base.Value = Math.Sqrt(value);
+                        break;
+                    case Przelicznik.Logarytmicznie:
+                        if (value == 0)
+
+                            base.Value = value;
+                        else
+                            base.Value = Math.Log10(value);
+                        break;
+                    default:
+                        base.Value = value;
+                        break;
+                }
+            }
+        }
+
+        public double _maks;
+        [Bindable(true)]
+        [Category("Własne")]
+        public double Max
+        {
+            get
+            {
+                switch (przelicznik)
+                {
+                    case Przelicznik.Liniowo:
+                        return base.Maximum;
+                    case Przelicznik.Kwadratowo:
+                        return base.Maximum * base.Maximum;
+                    case Przelicznik.Logarytmicznie:
+                        return Math.Pow(10, base.Maximum);
+                    default:
+                        return base.Maximum;
+                }
+            }
+            set
+            {
+                maksimumPrzelicz(value);
+                _maks = value;
+            }
+        }
+        void maksimumPrzelicz(double value)
+        {
+            switch (przelicznik)
+            {
+                case Przelicznik.Liniowo:
+                    base.Maximum = value;
+                    break;
+                case Przelicznik.Kwadratowo:
+                    base.Maximum = Math.Sqrt(value);
+                    break;
+                case Przelicznik.Logarytmicznie:
+                    if (value == 0)
+
+                        base.Maximum = value;
+                    else
+                        base.Maximum = Math.Log10(value);
+                    break;
+                default:
+                    base.Maximum = value;
+                    break;
+            }
+        }
+        [Bindable(true)]
+        [Category("Własne")]
+        public double Minimum
+        {
+            get
+            {
+                switch (przelicznik)
+                {
+                    case Przelicznik.Liniowo:
+                        return base.Minimum;
+                    case Przelicznik.Kwadratowo:
+                        return base.Minimum * base.Minimum;
+                    case Przelicznik.Logarytmicznie:
+                        return Math.Pow(10, base.Minimum);
+                    default:
+                        return base.Minimum;
+                }
+            }
+            set
+            {
+                switch (przelicznik)
+                {
+                    case Przelicznik.Liniowo:
+                        base.Minimum = value;
+                        break;
+                    case Przelicznik.Kwadratowo:
+                        base.Minimum = Math.Sqrt(value);
+                        break;
+                    case Przelicznik.Logarytmicznie:
+                        if (value == 0)
+
+                            base.Minimum = value;
+                        else
+                            base.Minimum = Math.Log10(value);
+                        break;
+                    default:
+                        base.Minimum = value;
+                        break;
+                }
+            }
         }
     }
 }
